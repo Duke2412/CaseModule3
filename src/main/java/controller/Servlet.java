a79ce83da55ffc43c6066822ef12e1bde0b870b1
LeDucTam2041992 <you@example.com>
2020-09-04 09:18:03 +0700

    first cm

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -1,19 +1,71 @@
 package controller;
 
+import dao.ProductDAO;
+import model.Product;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "Servlet")
+@WebServlet(name = "Servlet",urlPatterns = "/products")
 public class Servlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private ProductDAO productDAO;
 
+    public void init() {
+        productDAO = new ProductDAO();
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            default:
+                break;
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                listProducts(request, response);
+                break;
+        }
+    }
 
+    private void listProducts(HttpServletRequest request, HttpServletResponse response) {
+        List<Product> listProduct = productDAO.selectAlProduct();
+        request.setAttribute("listProduct", listProduct);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/listProduct.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
